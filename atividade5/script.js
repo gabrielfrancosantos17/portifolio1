0f8c1340db7734bc7a8090b68b8986bf6f2749fb
gabrielfrancosantos17 <gabriel.franco.santos17@escola.pr.gov.br>
2026-09-22 08:47:25 -0300

    Add files via upload

---
+++ atividade5/script.js
@@ -0,0 +1,13 @@
+function maior(){
+let numero1
+let numero2
+
+numero1 = Number (prompt("digite seu primeiro numero."));
+numero2 = Number (prompt("Digite seu segundo numero."));
+
+if(numero1 > numero2) {
+    alert("O primeiro numero é o maior.")
+}else{
+    alert ("O segundo numero é o maior.")
+}
+}
